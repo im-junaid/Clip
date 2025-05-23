8792499eab1f4d2c20bad9bf211163062fed4e6d
im-junaid <im.muhammedjunaid@gmail.com>
2025-05-23 22:57:22 +0530

    Prepare for Railway deployment

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate && python manage.py collectstatic --noinput
-web: gunicorn clip.wsgi --log-file - --timeout 120
+web: gunicorn clip.wsgi
--- clip/settings.py
+++ clip/settings.py
@@ -28,10 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-%23duqk)af1cd^5!j8x9a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-if os.environ.get('RAILWAY_PUBLIC_DOMAIN'):
-    ALLOWED_HOSTS.append(os.environ['RAILWAY_PUBLIC_DOMAIN'])
-
+ALLOWED_HOSTS = ["*"] 
 
 # Application definition
 
@@ -147,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = 'True'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'riderentals10@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'kxry neao zbtt ihzt')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = "Clip Support <riderentals10@gmail.com>"
 
 PASSWORD_RESET_EMAIL_HTML = 'emails/password_reset_email.html'
